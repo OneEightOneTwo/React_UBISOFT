09df527ed9ca53fd2f3167dd82c8204849b44e21
594097023 <594097023@qq.com>
2019-05-06 19:20:24 +0800

    car提交

--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ import { Router, Route, Switch } from 'dva/router';
 import IndexPage from './routes/IndexPage/IndexPage';
 import GamelistPage from './routes/GamelistPage/GamelistPage';
 import HomePage from './routes/HomePage/HomePage';
+import CarPage from './routes/CarPage/CarPage';
 
 function RouterConfig({ history }) {
   return (
@@ -11,6 +12,7 @@ function RouterConfig({ history }) {
         <Route path="/" exact component={IndexPage} />
         <Route path="/gamelist" exact component={GamelistPage} />
         <Route path="/home" exact component={HomePage} />
+        <Route path="/car" exact component={CarPage} />
       </Switch>
     </Router>
   );
